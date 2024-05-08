a0a1bc5063aa3cf28e934d50e7802d1f9d9af4a9
bigsteppa05 <habibforderiv05@gmail.com>
2024-05-08 21:05:17 +0300

    adding popular section function

--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -3,3 +3,23 @@ import { Link } from 'react-router-dom'
 import { useGlobalContext } from '../context/global'
 import styled from 'styled-components'
 import Sidebar from './Sidebar'
+
+function Popular({rendered}) {
+    const {popularAnime,isSearch, searchResults} = useGlobalContext()
+    
+
+    const conditionalRender = () => {
+        if(!isSearch && rendered === 'popular'){
+            return popularAnime?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }else{
+            return searchResults?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }
+    }
