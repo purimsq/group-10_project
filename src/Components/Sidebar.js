386b53cea868a421ed9ccb230acd70114cbb38d9
rogers.machara <rogersmachara13@gmail.com>
2024-05-08 22:38:36 +0300

    renders sidebar

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+import { useGlobalContext } from '../context/global';
+
+function Sidebar() {
+    return (
+        <SidebarStyled>
+        </SidebarStyled>
+    )
+}
+
+export default Sidebar;
