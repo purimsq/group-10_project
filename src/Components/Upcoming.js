137f7144ffa8e8ba572bb08b576934f960b08efb
paulsaitabau <paulsaitabau05@gmail.com>
2024-05-08 21:13:06 +0300

    Anime Upcoming Component

--- src/Components/Upcoming.js
+++ src/Components/Upcoming.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useGlobalContext } from '../context/global';
-import styled from 'styled-components';
 import Sidebar from './Sidebar';
 
 function Upcoming({ rendered }) {
