bb1a48ba28aca2264194210c4e80773cf843c984
rogers.machara <rogersmachara13@gmail.com>
2024-05-08 23:06:05 +0300

    sidebar errors

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -11,9 +11,23 @@ function Sidebar() {
     })
     
     return (
+        <>
         <SidebarStyled>
+        <h3>Top 5 Popular</h3>
+        <div className="anime">
+            {sorted?.slice(0,5).map((anime) => {
+                return <Link to={/anime/${anime.mal_id}} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                    <h5>
+                        {anime.title}
+                    </h5>
+                </Link>
+            })}
+        </div>
         </SidebarStyled>
-    )
+
+        </>
+            )
 }
 
 export default Sidebar;
