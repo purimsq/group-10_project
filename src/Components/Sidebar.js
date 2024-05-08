e3fc4f23a9585b2bfebfc7ec41d95cf46bc45b83
rogers.machara <rogersmachara13@gmail.com>
2024-05-08 22:47:12 +0300

    adds sort to sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,6 +4,12 @@ import styled from 'styled-components'
 import { useGlobalContext } from '../context/global';
 
 function Sidebar() {
+    const {popularAnime} = useGlobalContext()
+
+    const sorted = popularAnime?.sort((a,b) => {
+        return b.score - a.score
+    })
+    
     return (
         <SidebarStyled>
         </SidebarStyled>
