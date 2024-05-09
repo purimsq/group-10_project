2031297921d92f70bcdc3d09a6d692c052f24a2b
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-05-09 06:47:49 +0300

    progressed

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -30,3 +30,24 @@ function Gallery() {
             <div className="big-image">
                 <img src={pictures[index]?.jpg.image_url} alt="" />
             </div>
+            <div className="small-images">
+                {pictures?.map((picture, i) => {
+                    return <div className="image-con" onClick={()=>{
+                        handleImageClick(i)
+                    }} key={i}>
+                        <img 
+                            src={picture?.jpg.image_url}
+                            style={{
+                                border: i === index ? "3px solid #27AE60" : "3px solid #e5e7eb",
+                                filter: i === index ? 'grayscale(0)' : 'grayscale(60%)',
+                                transform: i === index ? 'scale(1.1)' : 'scale(1)',
+                                transition: 'all .3s ease-in-out'
+                            }}
+                            alt="" 
+                        />
+                    </div>
+                })}
+            </div>
+        </GalleryStyled>
+    )
+}
